ae46216d633cfe253326b0162e575190a4ca9bc3
daxhansen <you@weatherbill.com>
2014-07-31 09:11:26 -0600

    Daily Morning commit, too many changes to name

--- dist/assets/js/scripts.js
+++ dist/assets/js/scripts.js
@@ -12,6 +12,34 @@ document.querySelector("#nav-toggle").addEventListener("click", function() {
     this.classList.toggle("active");
 });
 
+/*  
+    Set up waypoints (Waypoints.js)
+    https://github.com/imakewebthings/jquery-waypoints
+
+*/
+
+$(document).ready(function(){
+    $('.waypoint-1').waypoint(function() {
+        $('.waypoint-1 #title').addClass('animated fadeInDown');
+        $('.waypoint-1 #sub-title').addClass('animated fadeInUp delay-short');
+    }, {
+        offset: '75%'
+    });
+
+    $('.waypoint-2').waypoint(function() {
+        $('#section-a h3').addClass('animated fadeInUp');
+        $('#section-a p').addClass('animated fadeInUp delay-short');
+    }, {
+        offset: '75%'
+    });
+     $('.waypoint-3').waypoint(function() {
+        $('.col-md-4 .photo-wrapper').addClass('animated zoomIn delay-short');
+    }, {
+        offset: '75%'
+    });
+});
+
+
 function bgParallax(el, rate, direction) {
     var $el = jQuery(el),
         offset = window.scrollY,
@@ -32,4 +60,14 @@ function bgParallax(el, rate, direction) {
         }
 }
 
-bgParallax('#top-hero', '0.6', 'up');
+
+$(window).on('scroll', function() {
+   window.requestAnimationFrame(scrollVent);
+});
+function scrollVent() {
+    //bgParallax('#top-hero', '0.8', 'up');
+}
+
+
+
+
