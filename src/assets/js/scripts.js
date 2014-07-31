b9f8fac38564c561e64aba95081bd05ac2a925eb
daxhansen <you@weatherbill.com>
2014-07-31 11:20:36 -0600

    Process section

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -12,13 +12,13 @@ document.querySelector("#nav-toggle").addEventListener("click", function() {
     this.classList.toggle("active");
 });
 
-/*  
-    Set up waypoints (Waypoints.js)
-    https://github.com/imakewebthings/jquery-waypoints
 
-*/
 
 $(document).ready(function(){
+    /*  
+    Set up waypoints (Waypoints.js)
+    https://github.com/imakewebthings/jquery-waypoints
+    */
     $('.waypoint-1').waypoint(function() {
         $('.waypoint-1 #title').addClass('animated fadeInDown');
         $('.waypoint-1 #sub-title').addClass('animated fadeInUp delay-short');
@@ -27,46 +27,32 @@ $(document).ready(function(){
     });
 
     $('.waypoint-2').waypoint(function() {
+        $('#section-a .anchor').addClass('animated zoomIn');
         $('#section-a h3').addClass('animated fadeInUp');
         $('#section-a p').addClass('animated fadeInUp delay-short');
     }, {
         offset: '75%'
     });
-     $('.waypoint-3').waypoint(function() {
-        $('.col-md-4 .photo-wrapper').addClass('animated zoomIn delay-short');
+    $('.waypoint-3').waypoint(function() {
+        $('.waypoint-3.photo-wrapper').addClass('animated zoomIn delay-short');
     }, {
         offset: '75%'
     });
+
+    $('.waypoint-4').waypoint(function() {
+        
+        $('.waypoint-4 h4').addClass('animated fadeIn');
+        $('.waypoint-4 p').addClass('animated fadeIn delay-short');
+        $('.waypoint-4 .large').addClass('animated fadeInDown');
+    }, {
+        offset: '75%'
+    });
+
+    /* instantiate Stellar.js (parallax) */
+    $(window).stellar();
 });
 
 
-function bgParallax(el, rate, direction) {
-    var $el = jQuery(el),
-        offset = window.scrollY,
-        $threshold = $el[0].offsetTop,
-        $height = $el.outerHeight(),
-        speed = rate * 0.125,
-        resetAmt = (($threshold-$height)*speed),
-
-        math = function(){
-            if (direction === 'up' || direction === null) {
-                return 50 - (offset * speed);
-            } else {
-                return 50 + (offset * speed);
-            }
-        };
-        if($el[0]) {
-            $el.css('background-position-y', math() + '%');
-        }
-}
-
-
-$(window).on('scroll', function() {
-   window.requestAnimationFrame(scrollVent);
-});
-function scrollVent() {
-    //bgParallax('#top-hero', '0.8', 'up');
-}
 
 
 
